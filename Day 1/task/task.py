d11fb82419eb16891652811dd5892d7d5fbbb40e
Komal.R.Aradhya <aradhyakomalak@gmail.com>
2026-03-05 14:27:07 +0530

    initial commit - Python project

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print ("Welcome to Band name generator")
+username = input("What is your name of the city you grew up in ?\n")
+pet_name = input("What is your pet name\n")
+print("Your band name could be " + username + " " +  pet_name)
