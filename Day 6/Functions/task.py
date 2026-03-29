d68cbdc5bdaf6b4986b4ccf21550421d6233c043
Komal.R.Aradhya <aradhyakomalak@gmail.com>
2026-03-30 01:06:40 +0530

    Day -6 ESCAPING THE HUZZLE

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,12 @@
+
+
+def my_function():
+    print("hello")
+  #  leng = len("hello ")
+   # print(leng)
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+
+my_function()
+get_user_name()
